e362280be405ce675372492593cfd3ac8f5ba4a3
ktquez <ktquez@gmail.com>
2016-10-12 19:27:40 -0300

    update config of assets from the dist folder of packages

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-  'source'     => app()->basePath('node_modules/react/react.js'),
-  'dom-source' => app()->basePath('node_modules/react-dom/index.js'),
-  'dom-server-source' => app()->basePath('node_modules/react-dom/server.js'),
+  'source'     => app()->basePath('node_modules/react/dist/react.js'),
+  'dom-source' => app()->basePath('node_modules/react-dom/dist/react-dom.js'),
+  'dom-server-source' => app()->basePath('node_modules/react-dom/dist/react-dom-server.js'),
   'components' => app()->basePath('public/js/components.js')
 ];
