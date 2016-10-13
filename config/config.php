a7fc64f0c572749c197ffc8ec0ebd9ddb6b8d60f
ktquez <ktquez@gmail.com>
2016-10-12 23:13:44 -0300

    Update helper base_path in config react.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-  'source'     => app()->basePath('node_modules/react/dist/react.js'),
-  'dom-source' => app()->basePath('node_modules/react-dom/dist/react-dom.js'),
-  'dom-server-source' => app()->basePath('node_modules/react-dom/dist/react-dom-server.js'),
-  'components' => app()->basePath('public/js/components.js')
+  'source'     => base_path('node_modules/react/dist/react.js'),
+  'dom-source' => base_path('node_modules/react-dom/dist/react-dom.js'),
+  'dom-server-source' => base_path('node_modules/react-dom/dist/react-dom-server.js'),
+  'components' => base_path('public/js/components.js')
 ];
